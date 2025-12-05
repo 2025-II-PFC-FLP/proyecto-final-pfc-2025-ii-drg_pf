258b4f1833e89a130cdf4c8ab8d5e5201223090a
NicoNGB7 <nicolas.gonzalez.bolanos@correounivalle.edu.co>
2025-12-05 01:34:54 -0500

    Empezamos Optimizacion con su primer funcion

--- app/src/main/scala/Proyecto/Optimizacion.scala
+++ app/src/main/scala/Proyecto/Optimizacion.scala
@@ -2,4 +2,14 @@ package Proyecto
 
 object Optimizacion {
 
+  /**
+   * Genera todas las programaciones posibles de tamaño n.
+   * Corresponde a las permutaciones de 0..n-1.
+   *
+   * @param n número de tablones
+   * @return todas las programaciones posibles
+   */
+  def todasLasProgramaciones(n: Int): Vector[ProRiego] = {
+    (0 until n).toVector.permutations.map(_.toVector).toVector // Permutaciones del dominio 0..n-1 convertidas a Vector
+  }
 }
