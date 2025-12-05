6717cc9b718c1915af94ec2c200d24299f4bf884
SebasG111 <sebastian.garcia.cardona@correounivalle.edu.co>
2025-12-04 23:08:40 -0500

    Se agregan las funciones en utilidades

--- app/src/main/scala/Proyecto/Utilidades.scala
+++ app/src/main/scala/Proyecto/Utilidades.scala
@@ -2,4 +2,24 @@ package Proyecto
 
 object Utilidades {
 
+  // i sirve para identificar al tablon en el vector de la Finca
+  // tspu obtiene el tiempo de supervivencia de un tablon en la Finca f
+  def tsup(f: Finca, i: Int): Int = {
+    f(i)._1
+  }
+
+
+  // treg obtiene el tiempo de regado de un tablon en la Finca f
+  def treg(f: Finca, i: Int): Int = {
+    f(i)._2
+  }
+
+
+  // prio obtiene el la prioridad de un tablon en la finca f
+  def prio(f: Finca, i: Int): Int = {
+    f(i)._3
+  }
+
+
+
 }
