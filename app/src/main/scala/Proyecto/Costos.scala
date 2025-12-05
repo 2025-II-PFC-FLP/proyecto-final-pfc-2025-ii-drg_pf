9abfd1bb6be33bf8e5360072b1223afab6f792c6
NicoNGB7 <nicolas.gonzalez.bolanos@correounivalle.edu.co>
2025-12-05 13:24:08 -0500

    paralelizacion de Costos

--- app/src/main/scala/Proyecto/Costos.scala
+++ app/src/main/scala/Proyecto/Costos.scala
@@ -1,8 +1,10 @@
 package Proyecto
+
 import Utilidades.tsup
 import Utilidades.treg
 import Utilidades.prio
 import TiempoRiego.tIR
+import common._
 
 object Costos {
 
@@ -28,7 +30,6 @@ object Costos {
   }
 
 
-
   def costoRiegoFinca(f: Finca, pi: ProRiego): Int = {
     //Recorre el vector Finca f de 0 a n-1 (n es el tamaño del vector o bien el numero de tablones)
     // i va tomando los valores de 0 a n-1 lo cual representa la posicion del tablon en el vector
@@ -36,12 +37,59 @@ object Costos {
   }
 
 
-
   def costoMovilidad(f: Finca, pi: ProRiego, d: Distancia): Int = {
     val parejas = pi.sliding(2).map{case Vector(a,b) => (a,b)}.toVector
     (0 until parejas.length).map(i => d(parejas(i)._1)(parejas(i)._2)).sum
   }
 
+
+
+
+  // VERSIONES PARALELAS
+
+
+  def costoRiegoFincaPar(f: Finca, pi: ProRiego): Int = {
+    val indices = (0 until f.length).toVector
+
+    // Para problemas grandes paralelizamos, para pequeños no (evitar overhead)
+    if (indices.length > 1000) {
+
+      val mitad = indices.length / 2
+
+      // Paralelizamos igual que en Programaciones
+      val (izq, der) = parallel(
+        indices.take(mitad).map(i => costoRiegoTablon(i, f, pi)),
+        indices.drop(mitad).map(i => costoRiegoTablon(i, f, pi))
+      )
+
+      izq.sum + der.sum
+
+    } else {
+      indices.map(i => costoRiegoTablon(i, f, pi)).sum
+    }
+  }
+
+
+
+  def costoMovilidadPar(f: Finca, pi: ProRiego, d: Distancia): Int = {
+    val parejas = pi.sliding(2).map{case Vector(a,b) => (a,b)}.toVector
+
+    // También igual que Programaciones: paralelizamos si hay suficiente trabajo
+    if (parejas.length > 1000) {
+
+      val mitad = parejas.length / 2
+
+      val (izq, der) = parallel(
+        parejas.take(mitad).map{ case (a,b) => d(a)(b) },
+        parejas.drop(mitad).map{ case (a,b) => d(a)(b) }
+      )
+
+      izq.sum + der.sum
+
+    } else {
+      parejas.map{ case (a,b) => d(a)(b) }.sum
+    }
+  }
 
 
 }
